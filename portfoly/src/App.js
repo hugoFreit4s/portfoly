91b9b431ced28c25b99710b69dbc0c6683293ce1
Hugo <hugo.encaminhados@gmail.com>
2024-07-14 19:03:47 -0300

    Video background adicionado

--- portfoly/src/App.js
+++ portfoly/src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="main">
-      <div className="profilepic">
-
-      </div>
-      <div className="projects">
-
-      </div>
-      <div className="social">
+      <video autoPlay loop muted className="bg-video">
+        <source src={process.env.PUBLIC_URL + '/bg-video.mp4'} type="video/mp4" />
+        Seu navegador não suporta vídeos HTML5.
+      </video>
+      <div className="square">
         
       </div>
     </div>
