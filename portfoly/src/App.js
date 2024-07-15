13e03cc8f9e76c341c5d834243cb85432c2e4bc6
hugoFreit4s <hugo.encaminhados@gmail.com>
2024-07-14 23:07:51 -0300

    Adicionando novos elementos visuais

--- portfoly/src/App.js
+++ portfoly/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 
 function App() {
+  function handleClick(){
+    console.log('I am clicked!');
+  }
+
   return (
     <div className="main">
       <video autoPlay loop muted className="bg-video">
@@ -8,7 +12,21 @@ function App() {
         Seu navegador não suporta vídeos HTML5.
       </video>
       <div className="square">
-        
+        <div className="profilepic">
+          <img id="profile-pic" src="profile-pic.jpg" />
+          <h1 id="name">Hugo de Freitas</h1>
+          <h2 id="title">Jr. Fullstack Developer</h2>
+        </div>
+        <div className="projects-social">
+          <div className="buttons">
+            <div className="button1">
+              <button onClick={handleClick}>PROJECTS</button>
+            </div>
+            <div className="button2">
+              <button>SOCIAL</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
